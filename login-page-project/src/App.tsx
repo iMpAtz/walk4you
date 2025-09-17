422a8f3b887eff58db665f0ee669bc6c0733bb65
Bento12ad <nongkhun2015@gmail.com>
2025-09-17 22:25:05 +0700

    Initial commit

---
+++ login-page-project/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginPage from './components/LoginPage';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Login Page</h1>
+            <LoginPage />
+        </div>
+    );
+};
+
+export default App;
